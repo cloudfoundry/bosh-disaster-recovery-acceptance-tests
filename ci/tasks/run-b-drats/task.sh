a2b2711d7e2eb4982aa1461eaf5556591d79758f
Diego Lemos <dlemos@vmware.com>
2021-10-11 09:59:46 +0100

    Make terraform and bosh-vars optional
    [#179721127]

--- ci/tasks/run-b-drats/task.sh
+++ ci/tasks/run-b-drats/task.sh
@@ -2,21 +2,17 @@
 
 set -eu
 
-jumpbox_ip="$( terraform output -state terraform-state/terraform.tfstate jumpbox-ip | jq -r .)"
+: "${JUMPBOX_IP:="$( terraform output -state terraform-state/terraform.tfstate jumpbox-ip | jq -r .)"}"
+: "${JUMPBOX_PRIVATE_KEY:="$( bosh interpolate --path /jumpbox_ssh/private_key "bosh-vars-store/${JUMPBOX_VARS_STORE_PATH}" )"}"
+: "${JUMPBOX_USER:=jumpbox}"
+
 jumpbox_private_key="$( mktemp )"
-bosh interpolate --path /jumpbox_ssh/private_key "bosh-vars-store/${JUMPBOX_VARS_STORE_PATH}" > "$jumpbox_private_key"
+echo -e $JUMPBOX_PRIVATE_KEY | sed -e 's/^"//' -e 's/"$//' > "$jumpbox_private_key"
 chmod 600 "$jumpbox_private_key"
-
-export BBR_BINARY_PATH="$( pwd )/$( ls bbr-binary-release/bbr-1*-linux-amd64 )"
-chmod +x "$BBR_BINARY_PATH"
-
-export GOPATH="$( pwd )"
-export PATH="${PATH}:${GOPATH}/bin"
-export INTEGRATION_CONFIG_PATH="$( pwd )/b-drats-integration-config/${INTEGRATION_CONFIG_PATH}"
 
 eval "$( ssh-agent )"
 ssh-add "$jumpbox_private_key"
-sshuttle -r "jumpbox@${jumpbox_ip}" "10.0.0.0/16" -D --pidfile=sshuttle.pid -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=${SSH_ALIVE_INTERVAL}"
+sshuttle -r "${JUMPBOX_USER}@${JUMPBOX_IP}" "10.0.0.0/16" -D --pidfile=sshuttle.pid -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=${SSH_ALIVE_INTERVAL}"
 sshuttle_pid="$( cat sshuttle.pid )"
 
 sleep 5
@@ -28,6 +24,12 @@ fi
 
 trap "kill ${sshuttle_pid}" EXIT
 
+export BBR_BINARY_PATH="$( pwd )/$( ls bbr-binary-release/bbr-1*-linux-amd64 )"
+chmod +x "$BBR_BINARY_PATH"
 
-./src/github.com/cloudfoundry-incubator/bosh-disaster-recovery-acceptance-tests/scripts/_run_acceptance_tests.sh
+export GOPATH="$( pwd )"
+export PATH="${PATH}:${GOPATH}/bin"
+export INTEGRATION_CONFIG_PATH="$( pwd )/b-drats-integration-config/${INTEGRATION_CONFIG_PATH}"
+
+./bosh-disaster-recovery-acceptance-tests/scripts/_run_acceptance_tests.sh
 
