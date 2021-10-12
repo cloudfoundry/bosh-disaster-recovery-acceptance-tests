974d1e05e91e0e5c9edeae34eac9e417928fdc5e
Diego Lemos <dlemos@vmware.com>
2021-10-12 14:33:21 +0100

    Rename task param

--- ci/tasks/run-b-drats/task.sh
+++ ci/tasks/run-b-drats/task.sh
@@ -5,10 +5,10 @@ set -eu
 : "${JUMPBOX_IP:="$( terraform output -state terraform-state/terraform.tfstate jumpbox-ip | jq -r .)"}"
 : "${JUMPBOX_PRIVATE_KEY:="$( bosh interpolate --path /jumpbox_ssh/private_key "bosh-vars-store/${JUMPBOX_VARS_STORE_PATH}" )"}"
 : "${JUMPBOX_USER:?}"
-: "${BBR_BINARY:?}"
+: "${BBR_BINARY_PATH:?}"
 
-chmod +x "$BBR_BINARY"
-export BBR_BINARY
+# chmod +x "$BBR_BINARY_PATH"
+# export BBR_BINARY_PATH
 
 jumpbox_private_key="$( mktemp )"
 echo -e "$JUMPBOX_PRIVATE_KEY" | sed -e 's/^"//' -e 's/"$//' > "$jumpbox_private_key"
