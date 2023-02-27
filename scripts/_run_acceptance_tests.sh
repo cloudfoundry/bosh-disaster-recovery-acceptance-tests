967a92ee1c8cfb7fe037ea1487faecba34f68c1a
Diego Lemos <dlemos@vmware.com>
2023-02-27 12:15:12 +0000

    Enhance Ginkgo timeout from 24h to 48h
    - This will allow for tests to run even when env being backed up and
      tests run in different regions (i.e. AMER and EMEA).

--- scripts/_run_acceptance_tests.sh
+++ scripts/_run_acceptance_tests.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-SCRIPTS_DIR="$(dirname $0)"
+SCRIPTS_DIR="$(dirname "$0")"
 pushd "$SCRIPTS_DIR/.."
   go mod tidy
-  ginkgo -v --trace "acceptance"
+  ginkgo -v --trace -timeout "$GINKGO_TIMEOUT" "acceptance"
 popd
