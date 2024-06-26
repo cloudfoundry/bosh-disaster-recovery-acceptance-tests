e649be343b54029467ad5963adacd1e031fd6058
Joseph Palermo <jpalermo@pivotal.io>
2024-06-25 18:40:18 -0700

    Revert "Remove bdrats ssh config. The tests should be doing jumpbox directly"
    This reverts commit 3a2433ecf4cb41761f282a476663012e4e963374.

--- ci/tasks/run-b-drats/task.sh
+++ ci/tasks/run-b-drats/task.sh
@@ -20,6 +20,20 @@ if cat $INTEGRATION_CONFIG_PATH | jq .jumpbox_host; then
   echo -e "$( cat $INTEGRATION_CONFIG_PATH | jq .jumpbox_privkey -r)" > ${PK_PATH}
 
   export CREDHUB_PROXY="ssh+socks5://${USER}@${HOST}:22?private-key=${PK_PATH}"
+
+  mkdir ~/.ssh
+  cat << EOF > ~/.ssh/config
+Host jumphost
+  HostName ${HOST}
+  User ${USER}
+  IdentityFile ${PK_PATH}
+
+  StrictHostKeyChecking no
+### Second jumphost. Only reachable via jumphost1.example.org
+Host ${BOSH_HOST}
+  HostName ${BOSH_HOST}
+  ProxyJump jumphost
+EOF
 fi
 
 ./bosh-disaster-recovery-acceptance-tests/scripts/_run_acceptance_tests.sh
